a1879724526cf76e5cddef6df336b172990e1247
David Taylor <david@taylorhq.com>
2022-12-29 12:32:58 +0000

    DEV: Introduce syntax_tree for ruby formatting (#9)

--- app/jobs/scheduled/update_marketo_leads.rb
+++ app/jobs/scheduled/update_marketo_leads.rb
@@ -9,7 +9,9 @@ module Jobs
 
       queue_size = Discourse.redis.llen(DiscourseMarketo::UPDATES_QUEUE)
       if queue_size > SiteSetting.marketo_updates_size
-        Rails.logger.warn("[UpdateMarketoLeads] Marketo user updates may be stuck in queue (current queue size = #{queue_size})")
+        Rails.logger.warn(
+          "[UpdateMarketoLeads] Marketo user updates may be stuck in queue (current queue size = #{queue_size})",
+        )
       end
 
       user_ids = []
@@ -21,24 +23,20 @@ module Jobs
 
       return if user_ids.blank?
 
-      input = User
-        .includes(:primary_email)
-        .where(id: user_ids)
-        .pluck(:email, :trust_level)
-        .map do |email, trust_level|
-        {
-          email: email,
-          SiteSetting.marketo_trust_level_field => trust_level,
-        }
-      end
+      input =
+        User
+          .includes(:primary_email)
+          .where(id: user_ids)
+          .pluck(:email, :trust_level)
+          .map do |email, trust_level|
+            { :email => email, SiteSetting.marketo_trust_level_field => trust_level }
+          end
 
       api = MarketoApi.new
       response = api.update_leads(input: input)
 
       if !response
-        user_ids.each do |user_id|
-          Discourse.redis.lpush(DiscourseMarketo::UPDATES_QUEUE, user_id)
-        end
+        user_ids.each { |user_id| Discourse.redis.lpush(DiscourseMarketo::UPDATES_QUEUE, user_id) }
       end
     end
   end
--- plugin.rb
+++ plugin.rb
@@ -9,29 +9,23 @@
 
 enabled_site_setting :discourse_marketo_enabled
 
-load File.expand_path('../lib/marketo_api.rb', __FILE__)
-load File.expand_path('../lib/validators/discourse_marketo_enabled_validator.rb', __FILE__)
+load File.expand_path("../lib/marketo_api.rb", __FILE__)
+load File.expand_path("../lib/validators/discourse_marketo_enabled_validator.rb", __FILE__)
 
 after_initialize do
   module ::DiscourseMarketo
-    PLUGIN_NAME = 'discourse-marketo'
-    UPDATES_QUEUE = 'marketo_updates'
+    PLUGIN_NAME = "discourse-marketo"
+    UPDATES_QUEUE = "marketo_updates"
   end
 
-  load File.expand_path('../app/jobs/regular/update_marketo_lead.rb', __FILE__)
-  load File.expand_path('../app/jobs/scheduled/update_marketo_leads.rb', __FILE__)
+  load File.expand_path("../app/jobs/regular/update_marketo_lead.rb", __FILE__)
+  load File.expand_path("../app/jobs/scheduled/update_marketo_leads.rb", __FILE__)
 
-  on(:user_created) do |user|
-    Jobs.enqueue_in(5.seconds, :update_marketo_lead, user_id: user.id)
-  end
+  on(:user_created) { |user| Jobs.enqueue_in(5.seconds, :update_marketo_lead, user_id: user.id) }
 
-  on(:user_updated) do |user|
-    Jobs.enqueue_in(5.seconds, :update_marketo_lead, user_id: user.id)
-  end
+  on(:user_updated) { |user| Jobs.enqueue_in(5.seconds, :update_marketo_lead, user_id: user.id) }
 
-  on(:user_destroyed) do |user|
-    Jobs.enqueue_in(5.seconds, :update_marketo_lead, user_id: user.id)
-  end
+  on(:user_destroyed) { |user| Jobs.enqueue_in(5.seconds, :update_marketo_lead, user_id: user.id) }
 
   on(:user_promoted) do |args|
     Discourse.redis.lpush(DiscourseMarketo::UPDATES_QUEUE, args[:user_id])
